52bbca5d69e618a91503510209b2559aa3db6bee
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-05-16 21:41:56 +0200

    exos-js
    Comparaisons

---
+++ comparaisons.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+    Quel sera le résultat pour les expressions suivantes :
+    ----------------------
+    5 > 4
+    "apple" > "pineapple"
+    "2" > "12"
+    undefined == null
+    undefined === null
+    null == "\n0\n"
+    null === +"\n0\n"
+    ----------------------
+*/
+
+5 > 4 // true 
+"apple" > "pineapple" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
