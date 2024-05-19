6e73a62e3be4c5382e134280fcdcfb0a0b4a1a7d
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-05-19 12:46:50 +0200

    exos-js
    Boucles while et for

--- while-for.js
+++ while-for.js
@@ -48,3 +48,35 @@ while (i) {
 */
 
 // préfix et postfix = de 0 à 4
+
+/*
+    Utilisez la boucle for pour afficher les nombres pairs de 2 à 10.
+*/
+
+for (let e = 2; e < 10; e++){
+    if ( i % 2 === 0 ) {
+        console.log(e)
+    }
+}
+
+/*
+    Réécrivez le code en modifiant la boucle for en while sans modifier son comportement (la sortie doit rester la même).
+    ------------------------------
+    for (let i = 0; i < 3; i++) {
+    alert( `number ${i}!` );
+    }
+    ------------------------------
+*/
+
+let i = 0;
+while (i < 3) {
+    alert(` number ${i}! `);
+    i++;
+}
+
+/*
+    Ecrivez une boucle qui demande un nombre supérieur à 100. Si le visiteur saisit un autre numéro, demandez-lui de le saisir à nouveau.
+    La boucle doit demander un numéro jusqu’à ce que le visiteur saisisse un nombre supérieur à 100 ou annule l’entrée/entre une ligne vide.
+    Ici, nous pouvons supposer que le visiteur ne saisit que des chiffres.
+    Il n’est pas nécessaire de mettre en œuvre un traitement spécial pour une entrée non numérique dans cette tâche.
+*/
