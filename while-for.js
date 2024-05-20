a4288938b3020c7572f225c6cad83a80941f3b9e
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-05-20 20:38:46 +0200

    exos-js
    Boucles while, for

--- while-for.js
+++ while-for.js
@@ -80,3 +80,29 @@ while (i < 3) {
     Ici, nous pouvons supposer que le visiteur ne saisit que des chiffres.
     Il n’est pas nécessaire de mettre en œuvre un traitement spécial pour une entrée non numérique dans cette tâche.
 */
+
+let num;
+
+do {
+    num = prompt("Enter a number greater than 100!", 0);
+}while (num <= 100 & num);
+
+/*
+    Un nombre entier supérieur à 1 est appelé un Nombre premier s’il ne peut être divisé sans reste par rien d’autre que 1 et lui-même.
+    En d’autres termes, n > 1 est un nombre premier s’il ne peut être divisé de manière égale par autre chose que 1 et n.
+    Par exemple, 5 est un nombre premier, car il ne peut pas être divisé sans reste par 2, 3 et 4.
+    Écrivez un code qui produit les nombres premiers dans l’intervall e 2 à n.
+    Pour n = 10, le résultat sera 2,3,5,7.
+    P.S. Le code devrait fonctionner pour n’importe quel n et aucune valeur fixe ne doit être codé en dur.
+*/
+
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+    }
+    alert(i);
+}
+
